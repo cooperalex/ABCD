d7ee021e4a4d6a3e7bc1646d6e58a048826dbbe5
Bob Perry <robert.a.perry@leidos.com>
2022-01-24 10:32:02 -0700

    updated settings to include apps.

--- abcd/abcd/settings.py
+++ abcd/abcd/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'abc_ingest.apps.AbcIngestConfig',
+    'abc_map.apps.AbcMapConfig',
+    'abc_eval.apps.AbcEvalConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
