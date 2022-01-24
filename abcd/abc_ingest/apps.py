940375d4aeb44b612af330f351e3d45f3fee5043
Bob Perry <robert.a.perry@leidos.com>
2022-01-24 10:24:05 -0700

    Added supporting django apps. The mapping will live in _map and the json ingest will live in _ingest. _eval is where some simple analysis will take place.

---
+++ abcd/abc_ingest/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AbcIngestConfig(AppConfig):
+    name = 'abc_ingest'
---
+++ abcd/abc_map/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AbcMapConfig(AppConfig):
+    name = 'abc_map'
